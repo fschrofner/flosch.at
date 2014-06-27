f88103ba492c5828c8ec24d8e9b943547a90eca5
Florian Schrofner <fschrofner@users.noreply.github.com>
2014-06-28 00:10:44 +0200

    fixed corrupted images, centered home page

--- index.js
+++ index.js
@@ -3,29 +3,47 @@ var markdown = require('metalsmith-markdown');
 var templates = require('metalsmith-templates');
 var collections = require('metalsmith-collections');
 var permalinks = require('metalsmith-permalinks');
+var branch = require('metalsmith-branch');
 var Handlebars = require('handlebars');
 var fs = require('fs');
 
 Handlebars.registerPartial('header',fs.readFileSync(__dirname + '/templates/partials/header.hbt').toString());
 Handlebars.registerPartial('footer',fs.readFileSync(__dirname + '/templates/partials/footer.hbt').toString());
 
-Metalsmith(__dirname)
-  .source('./src')
-  .destination('./build')
-  .use(collections({
-    pages: {
-      pattern: 'content/pages/*.md'
-    },
-    blog: {
-      pattern: 'content/blog/*/*/*.md',
-      sortBy: 'date',
-      reverse: true
-    }
-  }))
-  .use(permalinks({
-    pattern: ':collection/:title'
-  }))
-  .use(markdown())
+var metal = Metalsmith(__dirname);
+metal.source('./src');
+metal.destination('./build');
+
+metal.use(collections({
+  pages: {
+    pattern: 'content/pages/*.md'
+  },
+  blog: {
+    pattern: 'content/blog/*/*/*.md',
+    sortBy: 'date',
+    reverse: true
+  }
+}));
+  
+metal.use(permalinks({
+  pattern: ':collection/:title'
+}));
+
+metal.use(markdown({
+}));
+
+metal.use(branch(filterStaticContent)
   .use(templates('handlebars'))
-  .build()
+);
+
+metal.build();
+
+
+//this code part is needed since metalsmith-templates currently destroys binary files
+function filterStaticContent(filename, properties, index) {
+  var extension = filename.split('.').pop().toLowerCase();
+  var staticExtensions = [ "jpg", "jpeg", "png", "otf"];
+  var notStatic = staticExtensions.indexOf(extension) == -1;
+  return notStatic;
+}
   
--- src/content/pages/index.md
+++ src/content/pages/index.md
@@ -1,7 +1,24 @@
 ---
 title: Home
-template: default.hbt
+description: Welcome to my personal blog & portfolio!
+template: home.hbt
 ---
-Hi, this is my start page; neat ain't it? ;)
 
-###Some info!
+<p/>
+![It's a me!](/images/profile.png)
+
+#Welcome!
+My name is Florian Schrofner and I'm currently studying Mobile Computing at the University of Applied Sciences Hagenberg.  
+I've already developed mobile apps for Android and multiplatform using Qt, releasing all my work under open-source licences so far.  
+Welcome to my personal website (and soon blog) where you can find some information about me and my projects.  
+You should consider this website to be my virtual portfolio & playground.  
+
+<a href="https://www.facebook.com/florian.schrofner" target="_blank" class="icons">
+  <i class="fa fa-facebook-square fa-3x" id="fb-icon"></i>
+</a>
+<a href="https://google.com/+FlorianSchrofner/" target="_blank" class="icons">
+  <i class="fa fa-google-plus-square fa-3x" id="g-icon" style="margin-left:1rem"></i>
+</a>
+<a href="https://bitbucket.org/FlorianSchrofner" target="_blank" class="icons">
+  <i class="fa fa-bitbucket-square fa-3x"  id="bb-icon" style="margin-left:1rem"></i>
+</a>
