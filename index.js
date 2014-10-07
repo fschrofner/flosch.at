de80c7ef80578e98552b1702a96c6fb19659cb63
Florian Schrofner <fschrofner@users.noreply.github.com>
2014-10-07 18:56:49 +0200

    added tag functionality

--- index.js
+++ index.js
@@ -13,13 +13,16 @@ var fs = require('fs');
 Handlebars.registerPartial('header',fs.readFileSync(__dirname + '/templates/partials/header.hbt').toString());
 Handlebars.registerPartial('footer',fs.readFileSync(__dirname + '/templates/partials/footer.hbt').toString());
 Handlebars.registerPartial('blog_posts',fs.readFileSync(__dirname + '/templates/partials/blog_posts.hbt').toString());
+Handlebars.registerPartial('tags',fs.readFileSync(__dirname + '/templates/partials/tags.hbt').toString());
 Handlebars.registerPartial('post_header',fs.readFileSync(__dirname + '/templates/partials/post_header.hbt').toString());
 Handlebars.registerHelper('formatDate', formatDate);
+Handlebars.registerHelper('toLowerCase', toLowerCase);
 
 var metal = Metalsmith(__dirname);
 metal.source('./src');
 metal.destination('./build');
 
+//splits the sections
 metal.use(collections({
   pages: {
     pattern: 'content/pages/*.md'
@@ -38,6 +41,7 @@ metal.use(markdown());
 
 metal.use(excerpts());
 
+//create the correct permalinks for every section
 metal.use(branch(isPage)
   .use(permalinks({
     pattern: ':title'
@@ -55,18 +59,21 @@ metal.use(branch(isPost)
     pattern: ':collection/:date/:title'
   }))
 );
-
-metal.use(templates('handlebars'));
-
-metal
-    .use(tags({
-        handle: 'tags',                 // yaml key for tag list in you pages
-        path:'tag',                     // path for result pages
-        template:'/templates/tag.hbt',  // template to use for tag listing
-      sortBy: 'date',                   // provide posts sorted by 'date' (optional)
-      reverse: true                     // sort direction (optional)
-    }));
     
+//create tag pages for blogposts, must be called before the handlebars will be applied
+metal.use(tags({
+  handle: 'tags',                   // yaml key for tag list in you pages
+  path:'tag',                       // path for result pages
+  template:'../templates/tag.hbt',  // template to use for tag listing
+  sortBy: 'date',                   // provide posts sorted by 'date' (optional)
+  reverse: true                     // sort direction (optional)
+  })
+).use(permalinks({
+  pattern:'tag/:tag'
+}));
+    
+//finally applies handlebars
+metal.use(templates('handlebars'));
 
 metal.build();
 
@@ -94,6 +101,11 @@ function isPost(_filename, _properties, _index) {
   }
 }
 
+//these are handlebar helpers that will be used somewhere inside the handlebar files
 function formatDate(_datetime, _format) {
   return moment(new Date(_datetime)).format(_format);
+}
+
+function toLowerCase(_input){
+  return _input.toLowerCase();
 }
